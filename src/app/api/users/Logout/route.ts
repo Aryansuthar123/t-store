607a95e599bcec4b086507ebbc5bd8b43f5e764f
Aryansuthar123 <aryansuthar222@gmail.com>
2025-10-27 17:21:34 +0530

    all changiesss

--- src/app/api/users/Logout/route.ts
+++ src/app/api/users/Logout/route.ts
@@ -1,27 +1,29 @@
-    import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-    export async function POST() {
-    try {
-        const response = NextResponse.json({
-        success: true,
-        message: "Logged out successfully",
-        });
+export async function POST() {
+  try {
+    const response = NextResponse.json({
+      success: true,
+      message: "Logged out successfully",
+    });
 
-    
-        response.cookies.set("token", "", {
-        httpOnly: true,
-        expires: new Date(0),
-        });
 
-        return response;
-   } catch (error: unknown) {
-  const message =
-    error instanceof Error ? error.message : "Something went wrong";
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      secure: true,
+      path: "/",
+      expires: new Date(0),
+    });
 
-  return NextResponse.json(
-    { success: false, message },
-    { status: 500 }
-  );
+    return response;
+  } catch (error: unknown) {
+    const message =
+      error instanceof Error ? error.message : "Something went wrong";
+
+    return NextResponse.json(
+      { success: false, message },
+      { status: 500 }
+    );
+  }
+
 }
-
-    }
